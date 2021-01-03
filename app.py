5df9758abc2829a2f906532f2b8c40561bff6d02
Amogh Giri <amoghgiri01@gmail.com>
2021-01-03 17:23:40 -0500

    flask-react connection working

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+import json
+import numpy
 from flask import Flask
+from flask_cors import CORS
+from flask import request
 from joblib import load
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/home')
 def homepage():
     return {'result': "Hello World"}
 
-def spamClassify(text):
+@app.route('/results', methods = ['POST'])
+def spamClassify():
+    text = request.get_json()
     pipeline = load("spam_classification.joblib")
-    return pipeline.predict(text)
+    result = (pipeline.predict([text]))
+    return {'result': str(result[0])}
---
+++ react-app/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [placeholder, setPlaceholder] = useState('');
+
+  useEffect(() => {
+    fetch("/results", {
+      method:"POST",
+      cache: "no-cache",
+      headers:{
+          "content_type":"application/json",
+      },
+      body:JSON.stringify(placeholder)
+    }).then(res => res.json()).then(data => {
+      setPlaceholder(data.result);
+    });
+  }, [placeholder]);
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>Flask says that 'URGENT! Your Mobile No 1234 was awarded a Prize' is {placeholder}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
