16994d26c913b587385403f8d12b3439441d2a34
Amogh Giri <amoghgiri01@gmail.com>
2021-01-07 19:36:26 -0500

    restructured project

--- app.py
+++ app.py
@@ -5,12 +5,8 @@ from flask import request
 from joblib import load
 from spam import score
 
-app = Flask(__name__, static_folder='react-app/build', static_url_path='/react-app')
+app = Flask(__name__)
 CORS(app)
-
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
 
 @app.route('/results', methods = ['POST'])
 def spamClassify():
@@ -18,6 +14,3 @@ def spamClassify():
     pipeline = load("spam_classification.joblib")
     result = (pipeline.predict([text]))
     return {'result': str(result[0]), 'accuracy_score': score}
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
