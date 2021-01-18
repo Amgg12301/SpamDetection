56b47cde842c6bbf9b1a10579e12443bf172baca
Amogh Giri <amoghgiri01@gmail.com>
2021-01-18 16:51:09 -0500

    flask server ready

--- app.py
+++ app.py
@@ -5,12 +5,20 @@ from flask import request
 from joblib import load
 from spam import score
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./build', static_url_path='/')
 CORS(app)
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/results', methods = ['POST'])
 def spamClassify():
-    text = request.get_json()
+    text = request.get_json(force=True)
+    print(text)
     pipeline = load("spam_classification.joblib")
     result = (pipeline.predict([text]))
     return {'result': str(result[0]), 'accuracy_score': score}
+
+if __name__ == "__main__":
+    app.run(debug=True)
